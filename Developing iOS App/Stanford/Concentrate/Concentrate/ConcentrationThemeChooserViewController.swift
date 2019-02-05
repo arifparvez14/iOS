a1595b7455306290e53ef917beb7e9f99d009bf4
TheGenial <arifulislam14.ict@gmail.com>
2019-02-05 13:45:21 +0600

    Added Different type of Game :smiley:
    Added Different type of Game :smiley:

--- Developing iOS App/Stanford/Concentrate/Concentrate/ConcentrationThemeChooserViewController.swift
+++ Developing iOS App/Stanford/Concentrate/Concentrate/ConcentrationThemeChooserViewController.swift
@@ -10,21 +10,19 @@ import UIKit
 
 class ConcentrationThemeChooserViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
+    let themes = [
+        "Sports": "⚽️🏀🏈⚾️🥎🏐🎱🏓🎯🎳🎲🏂",
+        "Animal":"🐶🐓🦋🦇🦉🦀⛄️🐉🐙🐍🐙🦕",
+        "Faces":"😀😂😎🤪😕😩😷🤒😑😴",
+    ]
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "Choose Theme"{
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName]{
+                if let cvc = segue.destination as? ConcentrationViewController{
+                    cvc.theme = theme
+                }
+            }
+        }
     }
-    */
-
 }
--- Developing iOS App/Stanford/Concentrate/Concentrate/ConcentrationViewController.swift
+++ Developing iOS App/Stanford/Concentrate/Concentrate/ConcentrationViewController.swift
@@ -43,15 +43,17 @@ class ConcentrationViewController: UIViewController {
         }
     }
     private func updateViewFromModel(){
-        for index in cardButtons.indices{
-            let button = cardButtons[index]
-            let card = game.cards[index]
-            if card.isFaceUp{
-                button.setTitle(emoji(for: card), for: UIControl.State.normal)
-                button.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-            }else{
-                button.setTitle("", for: UIControl.State.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0) : #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+        if cardButtons != nil{
+            for index in cardButtons.indices{
+                let button = cardButtons[index]
+                let card = game.cards[index]
+                if card.isFaceUp{
+                    button.setTitle(emoji(for: card), for: UIControl.State.normal)
+                    button.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+                }else{
+                    button.setTitle("", for: UIControl.State.normal)
+                    button.backgroundColor = card.isMatched ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0) : #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+                }
             }
         }
     }
@@ -84,7 +86,7 @@ extension Int{
         if self > 0{
             return Int(arc4random_uniform(UInt32(self)))
         }else if self < 0{
-        return -Int(arc4random_uniform(UInt32(self)))
+            return -Int(arc4random_uniform(UInt32(self)))
         }else{
             return 0
         }
