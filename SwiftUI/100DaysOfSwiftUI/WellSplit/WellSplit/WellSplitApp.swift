5a9334b0951feaf16a9bca08f328cf500fa146e3
Muhammad Ariful Islam <arifparvez@sharetrip.net>
2021-04-04 12:20:19 +0600

    Add Test starter and 100 days of swift folder

---
+++ SwiftUI/100DaysOfSwiftUI/WellSplit/WellSplit/WellSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellSplitApp.swift
+//  WellSplit
+//
+//  Created by Sharetrip-iOS on 18/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct WellSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
