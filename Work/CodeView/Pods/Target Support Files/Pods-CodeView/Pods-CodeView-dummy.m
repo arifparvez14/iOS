7dd2553cbbc1f7c1de28d33f548b61bf469837bb
Muhammad Ariful Islam <muhammad.ariful@bjitgroup.com>
2023-06-05 09:51:36 +0600

    Remove extra files
    Remove extra files

--- Work/CodeView/Pods/Target Support Files/Pods-CodeView/Pods-CodeView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CodeView : NSObject
-@end
-@implementation PodsDummy_Pods_CodeView
-@end
--- Work/CodeView/Pods/Target Support Files/SwiftyCodeView/SwiftyCodeView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftyCodeView : NSObject
-@end
-@implementation PodsDummy_SwiftyCodeView
-@end
